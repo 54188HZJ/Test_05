ff9debc49d5e30ebc5d68c38ca8c960b92d6b425
Mack Ding <svip87@icloud.com>
2018-03-31 15:37:48 +0800

    first commit

---
+++ scripts/test_haha.py
@@ -0,0 +1,13 @@
+# -*- coding:utf-8 -*-
+class TestHa:
+
+    def test_haha1(self):
+        assert 1
+    def test_haha2(self):
+        assert 1
+    def test_haha3(self):
+        assert 1
+    def test_haha4(self):
+        assert 1
+    def test_haha5(self):
+        assert 0
